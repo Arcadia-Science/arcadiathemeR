e14a623b9eb4252462e9f9f6f8e07a59ad2a97dd
elizabethmcd <elizabethmcd93@gmail.com>
2024-06-10 14:12:40 -0700

    different fonts for numerical vs categorical

--- R/arcadia.R
+++ R/arcadia.R
@@ -1,7 +1,8 @@
 #' Arcadia theme adhering to illustration style guides
 #'
 #' @param base_size Default size of text in plot
-#' @param base_family Default font family in plot
+#' @param x_axis_type Type of the x-axis data("categorical" or "numerical")
+#' @param y_axis_type Type of the y-axis data("categorical" or "numerical")
 #'
 #' @return Layers onto an existing ggplot2 plot with a theme adhering to Arcadia style guidelines
 #' @export
@@ -19,17 +20,29 @@
 #'   geom_density(alpha=0.8) +
 #'   theme_arcadia()
 #' }
-theme_arcadia <- function(base_size = 12) {
-  base_family <- load_arcadia_font("SuisseIntl-Regular")
-  ggplot2::theme_minimal(base_size = base_size, base_family = base_family) +
+theme_arcadia <- function(base_size = 12, x_axis_type = "numerical", y_axis_type = "numerical") {
+  load_arcadia_font("SuisseIntlMono-Regular")
+  load_arcadia_font("SuisseIntl-Regular")
+
+  x_axis_family <- if (x_axis_type == "categorical") "SuisseIntl-Regular" else "SuisseIntlMono-Regular"
+  y_axis_family <- if (y_axis_type == "categorical") "SuisseIntl-Regular" else "SuisseIntlMono-Regular"
+  x_axis_label_family <- "SuisseIntl-Regular"
+  y_axis_label_family <- "SuisseIntl-Regular"
+
+  x_axis_ticks <- if (x_axis_type == "categorical") ggplot2::element_blank() else ggplot2::element_line(color="black", size = 0.5)
+  y_axis_ticks <- if (y_axis_type == "categorical") ggplot2::element_blank() else ggplot2::element_line(color="black", size = 0.5)
+
+  ggplot2::theme_minimal(base_size = base_size) +
 
     # font specifications
     ggplot2::theme(
-      plot.title = ggplot2::element_text(family = base_family, size = base_size * 1.2, face = "bold"),
-      axis.title = ggplot2::element_text(family = base_family, size = base_size),
-      axis.text = ggplot2::element_text(family = base_family, size = base_size * 0.8),
-      legend.title = ggplot2::element_text(family = base_family, size = base_size * 0.8),
-      legend.text = ggplot2::element_text(family = base_family, size = base_size * 0.7),
+      plot.title = ggplot2::element_text(family = "SuisseIntl-Regular", size = base_size * 1.2, face = "bold"),
+      axis.title.x = ggplot2::element_text(family = x_axis_label_family, size = base_size),
+      axis.title.y = ggplot2::element_text(family = y_axis_label_family, size = base_size),
+      axis.text.x = ggplot2::element_text(family = x_axis_family, size = base_size),
+      axis.text.y = ggplot2::element_text(family = y_axis_family, size = base_size),
+      legend.title = ggplot2::element_text(family = "SuisseIntl-Regular", size = base_size * 0.8),
+      legend.text = ggplot2::element_text(family = "SuisseIntl-Regular", size = base_size * 0.7),
 
       # background specifications
       plot.background = ggplot2::element_rect(fill = "#FDF8F2", color = NA),
@@ -39,8 +52,9 @@ theme_arcadia <- function(base_size = 12) {
       panel.grid.minor = ggplot2::element_blank(),
 
       # tick specifications
-      axis.ticks = ggplot2::element_line(color = "black", size = 0.5),
-      axis.line = ggplot2::element_line(color = "black", size = 0.5),
+      axis.ticks.x = x_axis_ticks,
+      axis.ticks.y = y_axis_ticks,
+      axis.line = ggplot2::element_line(color="black", size = 0.5),
 
       # legend specifications
       legend.background = ggplot2::element_rect(fill = "#FDF8F2", color = NA),
