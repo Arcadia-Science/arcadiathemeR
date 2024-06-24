e430ae4a0ef3dd550ed0aa95e401d6ec443f81ac
elizabethmcd <elizabethmcd93@gmail.com>
2024-06-24 14:03:40 -0700

    constants for fonts/colors

--- R/arcadia.R
+++ R/arcadia.R
@@ -4,6 +4,7 @@
 #' @param x_axis_type Type of the x-axis data("categorical" or "numerical")
 #' @param y_axis_type Type of the y-axis data("categorical" or "numerical")
 #'
+#'
 #' @return Layers onto an existing ggplot2 plot with a theme adhering to Arcadia style guidelines
 #' @export
 #'
@@ -20,10 +21,10 @@
 #'   geom_density(alpha=0.8) +
 #'   theme_arcadia()
 #' }
+
 theme_arcadia <- function(base_size = 12, x_axis_type = "numerical", y_axis_type = "numerical") {
   # load all Suisse fonts and store
   loaded_suisse_fonts <- load_arcadia_fonts("Suisse")
-  background_fill <- "#FDF8F2"
 
   # ensure fonts are loaded
   if (length(loaded_suisse_fonts) == 0) {
@@ -31,12 +32,12 @@ theme_arcadia <- function(base_size = 12, x_axis_type = "numerical", y_axis_type
     return(ggplot2::theme_minimal(base_size = base_size))
   }
 
-  x_axis_family <- if (x_axis_type == "categorical") "SuisseIntl-Regular" else "SuisseIntlMono-Regular"
-  y_axis_family <- if (y_axis_type == "categorical") "SuisseIntl-Regular" else "SuisseIntlMono-Regular"
-  x_axis_label_family <- "SuisseIntl-Regular"
-  y_axis_label_family <- "SuisseIntl-Regular"
-  legend_label_family <- "SuisseIntl-Regular"
-  legend_text_family <- if (x_axis_type == "categorical" || y_axis_type == "categorical") "SuisseIntl-Regular" else "SuisseIntlMono-Regular" # check both because depends on either to change the font
+  x_axis_family <- if (x_axis_type == "categorical") categorical_font else numerical_font
+  y_axis_family <- if (y_axis_type == "categorical") categorical_font else numerical_font
+  x_axis_label_family <- categorical_font
+  y_axis_label_family <- categorical_font
+  legend_label_family <- categorical_font
+  legend_text_family <- if (x_axis_type == "categorical" || y_axis_type == "categorical") categorical_font else numerical_font # check both because depends on either to change the font
 
 
   x_axis_ticks <- if (x_axis_type == "categorical") ggplot2::element_blank() else ggplot2::element_line(color="black", size = 0.5)
@@ -46,7 +47,7 @@ theme_arcadia <- function(base_size = 12, x_axis_type = "numerical", y_axis_type
 
     # font specifications
   ggplot2::theme(
-    plot.title = ggplot2::element_text(family = "SuisseIntl-Regular", size = base_size * 1.2, face = "bold"),
+    plot.title = ggplot2::element_text(family = regular_font, size = base_size * 1.2, face = "bold"),
     axis.title.x = ggplot2::element_text(family = x_axis_label_family, size = base_size),
     axis.title.y = ggplot2::element_text(family = y_axis_label_family, size = base_size),
     axis.text.x = ggplot2::element_text(family = x_axis_family, size = base_size),
@@ -72,3 +73,9 @@ theme_arcadia <- function(base_size = 12, x_axis_type = "numerical", y_axis_type
 
     )
 }
+
+# color and font constants
+background_fill <- "#FDF8F2"
+regular_font <- "SuisseIntl-Regular"
+categorical_font <- regular_font
+numerical_font <- "SuisseIntlMono-Regular"
