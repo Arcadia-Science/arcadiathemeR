41979ea3971ee4172a7b48fa8363425e1fc0a1df
Wasim Sandhu <wasim.sandhu@arcadiascience.com>
2024-11-04 19:25:14 -0800

    fix: Typo

--- R/arcadia.R
+++ R/arcadia.R
@@ -80,7 +80,7 @@ theme_arcadia <- function(x_axis_type = "numerical", y_axis_type = "numerical", 
 
 # font constants
 FONT <- "Suisse"
-if (!(custom_font %in% available_fonts)) {
+if (!(FONT %in% available_fonts)) {
   FONT <- system("fc-match -f '%%{family}' sans", intern = TRUE)
 }
 REGULAR_FONT <- paste(FONT, "Int'l")
