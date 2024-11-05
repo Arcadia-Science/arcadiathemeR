cbcaeb5fa477431b041722b81c20bd2dedc744f6
Wasim Sandhu <wasim.sandhu@arcadiascience.com>
2024-11-04 17:44:45 -0800

    fix: Import all fonts?

--- R/load_fonts.R
+++ R/load_fonts.R
@@ -1,3 +1,4 @@
+
 #' Load in custom fonts
 #'
 #' @param custom_font Font family name to be loaded in
@@ -11,8 +12,11 @@ load_arcadia_fonts <- function(custom_font = "Suisse", fallback_font = "sans") {
   font_names <- c("Suisse Int'l", "Suisse Int'l Semi Bold", "Suisse Int'l Medium", "Suisse Int'l Mono")
 
   # import and load fonts
-  invisible(utils::capture.output(extrafont::font_import(pattern = custom_font, prompt = FALSE)))
-  invisible(utils::capture.output(extrafont::loadfonts(device = "pdf", quiet = TRUE)))
+  # supress messages, below will handle incorrect loading
+  suppressMessages({
+    invisible(utils::capture.output(extrafont::font_import(prompt = FALSE)))
+    invisible(utils::capture.output(extrafont::loadfonts(device = "pdf", quiet = TRUE)))
+  })
 
   # check if custom font available
   available_fonts <- extrafont::fonts()
