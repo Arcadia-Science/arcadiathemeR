60ed913b93225bc353f8442415f684e8e202f7d8
Wasim Sandhu <wasim.sandhu@arcadiascience.com>
2024-11-04 17:57:03 -0800

    fix: Only import Suiss and default sans font

--- R/load_fonts.R
+++ R/load_fonts.R
@@ -10,11 +10,13 @@
 load_arcadia_fonts <- function(custom_font = "Suisse", fallback_font = "sans") {
   # define font names to check for
   font_names <- c("Suisse Int'l", "Suisse Int'l Semi Bold", "Suisse Int'l Medium", "Suisse Int'l Mono")
+  default_sans_font <- system("fc-match -f '%{family}' sans", intern = TRUE)
 
   # import and load fonts
   # supress messages, below will handle incorrect loading
   suppressMessages({
-    invisible(utils::capture.output(extrafont::font_import(prompt = FALSE)))
+    invisible(utils::capture.output(extrafont::font_import(pattern = custom_font, prompt = FALSE)))
+    invisible(utils::capture.output(extrafont::font_import(pattern = default_sans_font, prompt = FALSE)))
     invisible(utils::capture.output(extrafont::loadfonts(device = "pdf", quiet = TRUE)))
   })
 
